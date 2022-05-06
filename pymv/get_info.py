bab7446ac6ea27dd98df284c74a07bc51b956c18
David MacDonald <73856098+davidmacdonald80@users.noreply.github.com>
2022-05-06 17:59:39 -0500

    added Windows support
    Probably still a lot of bugs but it should work

--- pymv/get_info.py
+++ pymv/get_info.py
@@ -1,72 +1,116 @@
-import subprocess
 from pymediainfo import MediaInfo
+import re
 
-mkv_info = "/usr/bin/mkvinfo "
 
-
-def mkv_get_info(u):
-    h2 = "-1"
-    w2 = "-1"
-    hdv = str()
+def get_media_info(u):
+    height = "-1"
+    width = "-1"
     codec = "-1"
-    infchk = mkv_info + " " + str(u)
-    get_info = subprocess.Popen(infchk, shell=True, stdout=subprocess.PIPE)
-    get_info_return = get_info.stdout.read()
-    sort_info = get_info_return.decode("utf-8").split("\n")
-    for info in sort_info:
-        if (h2 != "-1") and (w2 != "-1") and (codec != "-1"):
-            break
-        elif info.startswith("(mkv_infonfo) No EBML head found."):
-            print("Error, not MKV or readable")
-            hdv = "Error"
-            codec = "Error"
-            continue
-        elif "Pixel width" in info:
-            winf1 = info.split(":")
-            w2 = winf1[1].lstrip()
-        elif "Pixel height" in info:
-            hinf1 = info.split(":")
-            h2 = hinf1[1].strip()
-        elif "Codec ID" in info:
-            # print(sort_info)
-            if "HEVC" in info:
-                # print("hevc: ", ino)
-                codec = "x265"
-            elif "AVC" in info:
-                codec = "x264"
-            else:
-                print("Video codec not identified")
-    hdv = w2 + "x" + h2
-    return hdv, codec
-
-
-def mp4_get_info(u):
-    h2 = "-1"
-    w2 = "-1"
-    hdv = str()
-    codec = "-1"
+    if re.match(r".+\.(?:mkv|m4v|mpg|mp4)$", str(u)):
+        for track in MediaInfo.parse(u).tracks:
+            if codec != "-1":
+                break
+            elif track.track_type == "Video":
+                codec = track.format
+    elif re.match(r".+\.(?:avi)$", str(u)):
+        for track in MediaInfo.parse(u).tracks:
+            if codec != "-1":
+                break
+            elif track.track_type == "Video":
+                codec = track.codec_id
+    else:
+        print("add extension and verify it pulls proper info")
+        print("pymv.getinfo - get_media_info")
+        quit()
     for track in MediaInfo.parse(u).tracks:
-        if (h2 != "-1") and (w2 != "-1") and (codec != "-1"):
+        if (height != "-1") and (width != "-1"):
             break
         elif track.track_type == "Video":
-            codec = track.format
-            h2 = track.height
-            w2 = track.width
-    hdv = str(w2) + "x" + str(h2)
-    return hdv, codec
+            height = track.height
+            width = track.width
+    resolution = str(width) + "x" + str(height)
+    if (height == "-1") or (width == "-1") or (codec == "-1"):
+        print("something is broken in pymv.get_info.py-get_media_info")
+        print("broke reading filename: {u}")
+        print("height is: {height}")
+        print("width is: {width}")
+        print("codec is: {codec}")
+        quit()
+    return resolution, codec
 
 
-def avi_get_info(u):
-    h2 = "-1"
-    w2 = "-1"
-    hdv = str()
-    codec = "-1"
-    for track in MediaInfo.parse(u).tracks:
-        if (h2 != "-1") and (w2 != "-1") and (codec != "-1"):
-            break
-        elif track.track_type == "Video":
-            codec = track.codec_id
-            h2 = track.height
-            w2 = track.width
-    hdv = str(w2) + "x" + str(h2)
-    return hdv, codec
+# def mkv_get_info(u):
+#     height = "-1"
+#     width = "-1"
+#     resolution = str()
+#     codec = "-1"
+#     for track in MediaInfo.parse(u).tracks:
+#         if (height != "-1") and (width != "-1") and (codec != "-1"):
+#             break
+#         elif track.track_type == "Video":
+#             codec = track.format
+#             height = track.height
+#             width = track.width
+#     resolution = str(width) + "x" + str(height)
+#     return resolution, codec
+# infchk = mkv_info + " " + str(u)
+# get_info = subprocess.Popen(infchk, shell=True, stdout=subprocess.PIPE)
+# get_info_return = get_info.stdout.read()
+# sort_info = get_info_return.decode("utf-8").split("\n")
+# for info in sort_info:
+#     if (height != "-1") and (width != "-1") and (codec != "-1"):
+#         break
+#     elif info.startswith("(mkv_infonfo) No EBML head found."):
+#         print("Error, not MKV or readable")
+#         resolution = "Error"
+#         codec = "Error"
+#         continue
+#     elif "Pixel width" in info:
+#         winf1 = info.split(":")
+#         width = winf1[1].lstrip()
+#     elif "Pixel height" in info:
+#         hinf1 = info.split(":")
+#         height = hinf1[1].strip()
+#     elif "Codec ID" in info:
+#         # print(sort_info)
+#         if "HEVC" in info:
+#             # print("hevc: ", ino)
+#             codec = "x265"
+#         elif "AVC" in info:
+#             codec = "x264"
+#         else:
+#             print("Video codec not identified")
+# resolution = width + "x" + height
+# return resolution, codec
+
+
+# def mp4_get_info(u):
+#     height = "-1"
+#     width = "-1"
+#     resolution = str()
+#     codec = "-1"
+#     for track in MediaInfo.parse(u).tracks:
+#         if (height != "-1") and (width != "-1") and (codec != "-1"):
+#             break
+#         elif track.track_type == "Video":
+#             codec = track.format
+#             height = track.height
+#             width = track.width
+#     resolution = str(width) + "x" + str(height)
+#     return resolution, codec
+
+
+# def avi_get_info(u):
+#     height = "-1"
+#     width = "-1"
+#     resolution = str()
+#     codec = "-1"
+#     for track in MediaInfo.parse(u).tracks:
+#         if (height != "-1") and (width != "-1") and (codec != "-1"):
+#             break
+#         elif track.track_type == "Video":
+#             codec = track.codec_id
+#             height = track.height
+#             width = track.width
+#     resolution = str(width) + "x" + str(height)
+#     return resolution, codec
